c4d28a170889c49001d41e3b6e338639c2a16324
ibrahimAlbayrak <47694762+AlbayrakStudios@users.noreply.github.com>
2020-12-07 18:11:33 +0300

    Update CameraController.cs

--- Player Camera and Movement/CameraController.cs
+++ Player Camera and Movement/CameraController.cs
@@ -8,17 +8,13 @@ public class CameraController : MonoBehaviour
     public float mouseSensitivity = 3f;
 
     public Image CursorImage;
-    public Sprite[] cursors;
 
     Transform player;
-    Transform hands;
 
-    bool isExamine;
     float camH, camV;
     void Start()
     {
         player = GameObject.FindObjectOfType<PlayerController>().transform;
-        hands = GameObject.FindWithTag("playerHand").transform;
 
         //set starup Rotate
         camH = transform.eulerAngles.x;
@@ -26,14 +22,12 @@ public class CameraController : MonoBehaviour
     }
     void Update()
     {
-        isExamine = GameObject.FindObjectOfType<Interact>().GetExamineState();
         CursorSetup();
     }
 
     void FixedUpdate()
     {
-        if(!GameObject.FindObjectOfType<GameManager>().stopGame)
-            Rotate();
+        Rotate();
     }
 
     void Rotate()
@@ -44,26 +38,12 @@ public class CameraController : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(camH,player.eulerAngles.y,0);
         player.rotation = Quaternion.Euler(0,camV,0);
-        hands.rotation = Quaternion.Euler(Mathf.Clamp(camH, -90, 25), camV, 0);
     }
 
     void CursorSetup()
     {
-        if (GameObject.FindObjectOfType<GameManager>().stopGame)
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         
-
-        if (isExamine)
-            CursorImage.sprite = cursors[1];
-        else
-            CursorImage.sprite = cursors[0];
     }
 }
