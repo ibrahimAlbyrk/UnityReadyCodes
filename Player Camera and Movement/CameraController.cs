a4247192726edc9748406f3ea3e8b71ae947c7a4
ibrahimAlbayrak <47694762+AlbayrakStudios@users.noreply.github.com>
2020-12-07 18:08:27 +0300

    Player and Camera Controller

---
+++ Player Camera and Movement/CameraController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 3f;
+
+    public Image CursorImage;
+    public Sprite[] cursors;
+
+    Transform player;
+    Transform hands;
+
+    bool isExamine;
+    float camH, camV;
+    void Start()
+    {
+        player = GameObject.FindObjectOfType<PlayerController>().transform;
+        hands = GameObject.FindWithTag("playerHand").transform;
+
+        //set starup Rotate
+        camH = transform.eulerAngles.x;
+        camV = player.eulerAngles.x;
+    }
+    void Update()
+    {
+        isExamine = GameObject.FindObjectOfType<Interact>().GetExamineState();
+        CursorSetup();
+    }
+
+    void FixedUpdate()
+    {
+        if(!GameObject.FindObjectOfType<GameManager>().stopGame)
+            Rotate();
+    }
+
+    void Rotate()
+    {
+        camH += Input.GetAxisRaw("Mouse Y") * -mouseSensitivity;
+        camV += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camH = Mathf.Clamp(camH, -85, 75);
+
+        transform.rotation = Quaternion.Euler(camH,player.eulerAngles.y,0);
+        player.rotation = Quaternion.Euler(0,camV,0);
+        hands.rotation = Quaternion.Euler(Mathf.Clamp(camH, -90, 25), camV, 0);
+    }
+
+    void CursorSetup()
+    {
+        if (GameObject.FindObjectOfType<GameManager>().stopGame)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        
+
+        if (isExamine)
+            CursorImage.sprite = cursors[1];
+        else
+            CursorImage.sprite = cursors[0];
+    }
+}
---
+++ Player Camera and Movement/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [Header("Movement")]
+    public float walkSpeed;
+    public float runSpeed;
+    public float jumpForce;
+
+    public Transform groundDetectPos;
+
+    float speed;
+    bool isRun;
+    bool isGrounded;
+
+    [Header("FootStep")]
+    public AudioClip[] concreteSteps;
+    public AudioClip[] woodSteps;
+
+    string groundTag;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(!GameObject.FindObjectOfType<GameManager>().stopGame)
+        {
+            Movement();
+            Footstep();
+        }
+        else
+            speed = 0;
+    }
+
+    void Movement()
+    {
+
+        // walk
+        if (isRun)
+            speed = Mathf.Lerp(speed,runSpeed,.07f);
+        else
+            speed = Mathf.Lerp(speed, walkSpeed, .07f);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 rot = transform.TransformDirection(new Vector3(x,0,z)).normalized;
+
+        rb.position += rot * speed * Time.deltaTime;
+
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            StartCoroutine(jump());
+        }
+
+        //ground Detect
+        isGrounded = groundDetector();
+
+        //run
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            isRun = true;
+        else
+            isRun = false;
+
+        //Funcs
+        bool groundDetector()
+        {
+
+            Collider[] colls = Physics.OverlapSphere(groundDetectPos.position, .1f);
+            foreach (Collider coll in colls)
+            {
+                groundTag = coll.gameObject.tag;
+                if (coll.gameObject.tag == "ground")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        IEnumerator jump()
+        {
+            yield return new WaitForSeconds(.15f);
+            rb.AddForce(transform.up * jumpForce);
+        }
+
+    }
+    void Footstep()
+    {
+
+    }
+}
